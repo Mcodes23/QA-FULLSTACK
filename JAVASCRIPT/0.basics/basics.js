5a719d82e705534aa020cd4e863e69f10d87f705
Mcodes23 <mateliboniface@gmail.com>
2025-02-11 15:49:05 +0300

    new task

--- JAVASCRIPT/0.basics/basics.js
+++ JAVASCRIPT/0.basics/basics.js
@@ -1,1 +1,10 @@
-console.log("hello")
+const myGrade1 = (mark) => {
+    if (mark < 0 || mark > 100) return 'invalid input';
+    return mark === 100 ? 'A' :
+           mark >= 70 ? 'B' :
+           mark >= 50 ? 'C' :
+           mark >= 30 ? 'D' :
+           'F';
+}
+
+console.log(myGrade1(657))
